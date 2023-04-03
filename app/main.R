ee4e1827c23db4ce23fccc23a5dd640d9a2da4ef
Marc <marccocamoreno@gmail.com>
2023-04-03 20:57:29 +0200

    + Navbar and pseudonavigation

--- app/main.R
+++ app/main.R
@@ -1,14 +1,26 @@
 box::use(
-  shiny[bootstrapPage, moduleServer, NS, renderText, tags, textOutput],
+  shiny[bootstrapPage, moduleServer, NS, renderText, tags, textOutput,navbarPage],
+  bslib[page_navbar,nav,nav_menu,bs_theme,card,card_body,card_body_fill,font_google]
 )
+
+# App main theme
+karyoploter_theme = bs_theme(
+  version = "5",
+  base_font = font_google("Roboto"),
+  bootswatch = "zephyr"
+)
+
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  bootstrapPage(
-    tags$h3(
-      textOutput(ns("message"))
-    )
+  navbarPage(
+    theme = karyoploter_theme,
+    title = "KaryoploteR",
+    inverse = FALSE,
+    nav("Link 1"),
+    nav("Link 2")
+
   )
 }
 
