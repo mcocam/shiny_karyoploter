ce5e8838918589da677166ec9ca27f5967371b06
Marc <marccocamoreno@gmail.com>
2023-04-03 20:21:19 +0200

    + Dependencies: Cairo, dplyr and bslib

--- dependencies.R
+++ dependencies.R
@@ -1,3 +1,7 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
-library(rhino)
-library(karyoploteR)
+
+library(bslib)        # Bootstrap 5 set up
+library(Cairo)        # Export plot as image
+library(dplyr)        # Manipulate data
+library(karyoploteR)  # Generate karyotype plots
+library(rhino)        # Shiny Framework
